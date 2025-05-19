9615b0a77e3bb509723400eb3e3352835e3bb41a
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2025-05-19 13:57:53 -0700

    landing done

---
+++ preply/frontend/src/app/landing/page.tsx
@@ -0,0 +1,40 @@
+import Footer from "../components/footer";
+import Image from 'next/image';
+import logo from '../assests/logoTransparent.png';
+
+export default function Landing() {
+  return (
+    <div
+      className="w-screen h-screen bg-cover bg-center overflow-x-hidden"
+      style={{ backgroundImage: "url('/landingPhoto.jpg')" }}
+    >
+      <div className="flex flex-col items-center justify-center h-screen relative">
+        {/* Centered yellow box */}
+        <div className="relative">
+          <div className="bg-[#E9C06B] w-[60vw] lg:w-[15vw] h-[30vh] rounded-[30px] opacity-80"></div>
+
+          {/* Logo overlapping center of yellow box */}
+          <Image
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
+            src={logo}
+            alt="Logo"
+            width={175}
+            height={175}
+            priority
+          />
+        </div>
+
+        {/* Buttons below */}
+        <div className="mt-12 flex gap-6">
+          <button className="w-24 h-12 bg-[#E9C06B] opacity-80 rounded-[15px] text-white">Login</button>
+          <button className="w-24 h-12 bg-[#E9C06B] opacity-80 rounded-[15px] text-white">Sign Up</button>
+        </div>
+      </div>
+
+      {/* Footer pushed off screen for animation or later use */}
+      <div className="absolute -bottom-60 w-full">
+        <Footer />
+      </div>
+    </div>
+  );
+}
